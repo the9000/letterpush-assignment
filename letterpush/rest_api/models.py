eca99ac7eb2519f139269fc3e5739866295e41ec
Dmitry Cheryasv <dmitry.cheryasov@gmail.com>
2016-10-04 11:28:25 -0400

    Embedded image resource.

--- letterpush/rest_api/models.py
+++ letterpush/rest_api/models.py
@@ -21,6 +21,18 @@ class Article(DateTrackingModel):
     # NOTE: in a real project, body would be stored differently.
     body = models.CharField(max_length=2000)
 
+    def imagesByRole(self):
+        """
+        Eagerly load and group images as a dict keyed by role.
+        Ignores link info except the role name.
+        """
+        links = {role: [] for role in ImageLink.ROLES}
+        role_label = dict(ImageLink.ROLE_CHOICES)  # Map .role value to label.
+        for image_link in self.imagelink_set.all().prefetch_related("image"):
+            links[role_label[image_link.role]].append(image_link.image)
+        return links
+
+
 
 class Image(DateTrackingModel):
     """Stores an image meta info."""
--- letterpush/rest_api/resources.py
+++ letterpush/rest_api/resources.py
@@ -9,13 +9,23 @@ from .models import Article, Image, ImageLink
 
 
 class ArticleResource(DjangoResource):
-    preparer = FieldsPreparer(fields={
+    preparer = FieldsPreparer(fields={  # Bare minimum of properties.
         'id': 'id',
         'title': 'title',
         'body': 'body',
         'created': 'created',
         'updated': 'updated',
     })
+
+    def prepare(self, inatance):
+        """Turns a model into a serializable representation."""
+        prepared = super(self.__class__, self).prepare(inatance)
+        # Embed image resources as nested.
+        img_res = ImageResource()
+        prepared_images = {role: map(img_res.prepare, images)
+                           for role, images in inatance.imagesByRole().items()}
+        prepared["images"] = prepared_images
+        return prepared
 
     # GET /
     def list(self):
@@ -24,3 +34,16 @@ class ArticleResource(DjangoResource):
     # GET /<pk>/
     def detail(self, pk):
         return Article.objects.get(id=pk)
+
+
+class ImageResource(DjangoResource):
+    preparer = FieldsPreparer(fields={  # Bare minimum of properties.
+        'id': 'id',
+        'note': 'note',
+        'path': 'path',
+        'created': 'created',
+        'updated': 'updated',
+    })
+
+    def list(self):
+        return Image.objects.all()
